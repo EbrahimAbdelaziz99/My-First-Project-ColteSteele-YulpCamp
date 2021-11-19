7e80ac74e0917a98662db22d553caf38f70b45c3
Ebrahim Abdelaziz <pipozizo22@gmail.com>
2021-11-19 10:24:05 +0200

    final change

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
 
-const dbUrl = peocess.env.DB_URL ||'mongodb://localhost:27017/yelp-camp' ; 
+const dbUrl = process.env.DB_URL ||'mongodb://localhost:27017/yelp-camp' ; 
 
 mongoose.connect( dbUrl , {
     useNewUrlParser: true,
