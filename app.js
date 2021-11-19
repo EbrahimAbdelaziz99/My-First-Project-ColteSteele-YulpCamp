234eccb21d362a3e0f0bf5b1ec064d6008dff143
Ebrahim Abdelaziz <pipozizo22@gmail.com>
2021-11-19 09:57:51 +0200

    final change

--- app.js
+++ app.js
@@ -40,20 +40,20 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({
     replaceWith: '_'
-}))
+}));
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!',
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-let store = MongoStore.create({
+const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
@@ -77,7 +77,7 @@ const sessionConfig = {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}
+};
 
 app.use(session(sessionConfig));
 app.use(flash());
@@ -141,12 +141,12 @@ app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
-})
+});
 
 
 app.use('/', userRoutes);
-app.use('/campgrounds', campgroundRoutes)
-app.use('/campgrounds/:id/reviews', reviewRoutes)
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 
 app.get('/', (req, res) => {
@@ -156,17 +156,17 @@ app.get('/', (req, res) => {
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
-})
+});
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
-})
+});
 
 const port = process.env.PORT || 3000 ;
 
 app.listen(port , () => {
     console.log(`Serving on port ${port}`)
-}) 
+});
 
