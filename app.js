15b50ed6672afad1126d95eedc7a7bd3d226ab75
HiMa36 <eabdelaziz3699@gmail.com>
2021-11-21 12:49:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require("connect-mongo");
 
-const dbUrl = process.env.DB-URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
